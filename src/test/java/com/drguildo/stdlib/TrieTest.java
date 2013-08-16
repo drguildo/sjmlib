8d66e38d05921a2ebc21c4cd80465546c0f98856
Simon Morgan <sjm@eml.cc>
2013-08-16 20:13:11 +0100

    Added another test.

--- src/test/java/com/drguildo/stdlib/TrieTest.java
+++ src/test/java/com/drguildo/stdlib/TrieTest.java
@@ -1,10 +1,19 @@
 package com.drguildo.stdlib;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import org.junit.Test;
 
 public class TrieTest {
+  private static final String DICT_PATH = "src/test/resources/2of4brif.txt";
+
   @Test
   public void testContains() {
     Trie t = new Trie();
@@ -23,5 +32,24 @@ public class TrieTest {
     assertFalse(t.contains(""));
     assertFalse(t.contains("abc"));
     assertFalse(t.contains("abcde"));
+  }
+
+  @Test
+  public void testAdd() throws IOException {
+    Trie t = new Trie();
+
+    List<String> lines = Files.readAllLines(Paths.get(DICT_PATH),
+        Charset.forName("UTF-8"));
+
+    for (String line : lines) {
+      t.add(line);
+    }
+
+    assertTrue(t.contains("aah"));
+    assertTrue(t.contains("aardvark"));
+
+    assertFalse(t.contains("xaardvark"));
+    assertFalse(t.contains("aardvarkx"));
+    assertFalse(t.contains("aardxvark"));
   }
 }
