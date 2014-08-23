ae4f730bfa10a8c3f14181cb5cb965984c7410fc
Simon Morgan <sjm@sjm.io>
2014-08-23 22:13:34 +0100

    Import new file structure.

---
+++ src/com/drguildo/stdlib/benchmarks/TrieBenchmark.java
@@ -0,0 +1,52 @@
+package com.drguildo.stdlib.benchmarks;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.drguildo.stdlib.Trie;
+
+public class TrieBenchmark {
+  private static final String DICT_PATH = "src/test/resources/2of4brif.txt";
+
+  public static void main(String[] args) throws IOException {
+    ArrayList<String> a = new ArrayList<String>();
+    Trie t = new Trie();
+
+    List<String> lines = Files.readAllLines(Paths.get(DICT_PATH),
+        Charset.forName("UTF-8"));
+
+    System.out.println("dictionary size: " + lines.size());
+
+    long startTime = System.currentTimeMillis();
+    for (String line : lines) {
+      a.add(line);
+    }
+    long endTime = System.currentTimeMillis();
+    System.out.println("ArrayList add: " + (endTime - startTime) + "ms");
+
+    startTime = System.currentTimeMillis();
+    for (String line : lines) {
+      t.add(line);
+    }
+    endTime = System.currentTimeMillis();
+    System.out.println("Trie add: " + (endTime - startTime) + "ms");
+
+    startTime = System.currentTimeMillis();
+    for (String line : lines) {
+      a.contains(line);
+    }
+    endTime = System.currentTimeMillis();
+    System.out.println("ArrayList lookup: " + (endTime - startTime) + "ms");
+
+    startTime = System.currentTimeMillis();
+    for (String line : lines) {
+      t.contains(line);
+    }
+    endTime = System.currentTimeMillis();
+    System.out.println("Trie lookup: " + (endTime - startTime) + "ms");
+  }
+}
