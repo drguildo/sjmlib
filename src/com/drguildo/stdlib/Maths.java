959502e6b90066041e2691c764eaeeb821e3f2f5
Simon Morgan <sjm@sjm.io>
2015-02-02 17:12:03 +0000

    Added a method for when you want to divide two integers but want the result to be rational.

--- src/com/drguildo/stdlib/Maths.java
+++ src/com/drguildo/stdlib/Maths.java
@@ -4,4 +4,8 @@ public class Maths {
   public static int gcd(int x, int y) {
     return y == 0 ? x : gcd(y, x % y);
   }
+
+  public static float div(int a, int b) {
+    return (float) a / (float) b;
+  }
 }
