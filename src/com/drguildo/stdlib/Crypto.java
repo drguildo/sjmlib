e30d3e56b29765a64489110066f03fc45646d9a6
Simon Morgan <sjm@sjm.io>
2015-04-12 17:46:02 +0100

    Added a simple helper class for encryption and decryption and accompanying tests.

---
+++ src/com/drguildo/stdlib/Crypto.java
@@ -0,0 +1,54 @@
+package com.drguildo.stdlib;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.util.Base64;
+
+/**
+ * A simple helper class for encryption and decryption.
+ */
+public class Crypto {
+  private final static String CIPHER = "Blowfish";
+  private final static String CHAR_ENCODING = "UTF-8";
+
+  /**
+   * Encrypt the given string using Blowfish and returns the Base64 encoded
+   * result.
+   *
+   * @param plainText the text to encode
+   * @param encryptionKey the encryption key
+   * @return the Bases64 encoded result of encrypting the plaintext
+   * @throws GeneralSecurityException
+   */
+  public static String encryptString(String plainText, String encryptionKey) throws GeneralSecurityException {
+    byte[] encrypted;
+
+    SecretKeySpec secretKeySpec = new SecretKeySpec(encryptionKey.getBytes(), CIPHER);
+    Cipher cipherInstance = Cipher.getInstance(CIPHER);
+
+    cipherInstance.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+    encrypted = cipherInstance.doFinal(plainText.getBytes());
+
+    return Base64.getEncoder().encodeToString(encrypted);
+  }
+
+  /**
+   * Decrypts the given Base64 encoded Blowfish ciphertext.
+   *
+   * @param cipherText the text to decode
+   * @param encryptionKey the decryption key
+   * @return the Base64 decoded result of decrypting the ciphertext
+   * @throws GeneralSecurityException
+   * @throws UnsupportedEncodingException
+   */
+  public static String decryptString(String cipherText, String encryptionKey) throws GeneralSecurityException, UnsupportedEncodingException {
+    Cipher cipherInstance = Cipher.getInstance(CIPHER);
+    SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes(CHAR_ENCODING), CIPHER);
+
+    cipherInstance.init(Cipher.DECRYPT_MODE, key);
+
+    return new String(cipherInstance.doFinal(Base64.getDecoder().decode(cipherText)), CHAR_ENCODING);
+  }
+}
