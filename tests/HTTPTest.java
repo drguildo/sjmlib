9adb2d85d416a62f53e301a3c6bbee52c206fd9f
Simon Morgan <sjm@sjm.io>
2015-04-12 16:10:30 +0100

    Move HTTP tests.

---
+++ tests/HTTPTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+import com.drguildo.stdlib.HTTP;
+
+public class HTTPTest {
+  @Test
+  public void test() {
+    try {
+      HTTP.download(new URL("http://ipv4.download.thinkbroadband.com/5MB.zip"));
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+}
