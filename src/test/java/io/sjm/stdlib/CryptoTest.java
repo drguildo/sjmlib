791e1076dd0dfe3a77371535792e532c765a1adc
Simon Morgan <sjm@sjm.io>
2015-04-16 17:26:12 +0100

    Add some tests for Crypto.hash().

--- src/test/java/io/sjm/stdlib/CryptoTest.java
+++ src/test/java/io/sjm/stdlib/CryptoTest.java
@@ -33,4 +33,12 @@ public class CryptoTest {
 
     assertEquals(str, Crypto.decryptString(encryptedStr, key));
   }
+
+  @Test
+  public void testHash() throws NoSuchAlgorithmException {
+    assertEquals(Crypto.hash("test", "MD5"), "098f6bcd4621d373cade4e832627b4f6");
+    assertEquals(Crypto.hash("test", "SHA-1"), "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3");
+    assertEquals(Crypto.hash("test", "SHA-256"),
+        "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08");
+  }
 }
