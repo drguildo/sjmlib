9a0ce9296cd720dd1b448ed97b164cdc7b16d905
Simon Morgan <sjm@sjm.io>
2015-04-16 17:21:13 +0100

    assertNotEquals in testDecryptString should be assertEquals.

--- src/test/java/io/sjm/stdlib/CryptoTest.java
+++ src/test/java/io/sjm/stdlib/CryptoTest.java
@@ -2,11 +2,12 @@ package io.sjm.stdlib;
 
 import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class CryptoTest {
   Rand rand;
@@ -30,6 +31,6 @@ public class CryptoTest {
     String key = rand.randString(12);
     String encryptedStr = Crypto.encryptString(str, key);
 
-    assertNotEquals(str, Crypto.decryptString(encryptedStr, key));
+    assertEquals(str, Crypto.decryptString(encryptedStr, key));
   }
 }
