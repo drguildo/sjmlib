81faeed70836a310b9eb9d3167aa5ffce2090c38
Simon Morgan <sjm@sjm.io>
2015-04-23 15:01:41 +0100

    Add some lorem ipsum goodness.

--- src/main/java/io/sjm/stdlib/Strings.java
+++ src/main/java/io/sjm/stdlib/Strings.java
@@ -6,6 +6,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Strings {
+  public static final String LOREM =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
   /**
    * Searches a CharSequence for instances of /pattern/ and returns them.
    */
