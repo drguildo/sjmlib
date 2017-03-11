126bc0e4b39a4bc8462cf72b0c7f7d7f0d1c6d6a
Simon Morgan <sjm@sjm.io>
2017-03-11 20:49:32 +0000

    Add some new convenience methods

--- src/main/java/io/sjm/sjmlib/Arrays.java
+++ src/main/java/io/sjm/sjmlib/Arrays.java
@@ -1,5 +1,6 @@
 package io.sjm.sjmlib;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -101,7 +102,7 @@ public class Arrays {
 
   /**
    * Finds the largest value in an array of doubles.
-   * 
+   *
    * @param a the array of doubles
    * @return the largest value in the array
    */
@@ -121,7 +122,7 @@ public class Arrays {
 
   /**
    * Finds the largest value in an array of integers.
-   * 
+   *
    * @param a the array of integers
    * @return the largest value in the array
    */
@@ -137,6 +138,117 @@ public class Arrays {
           m = a[i];
 
     return m;
+  }
+
+  /**
+   * A convenience method for converting an array of bytes to an ArrayList of byte objects.
+   *
+   * @param elems a primitive array of bytes
+   * @return an ArrayList of Byte objects
+   */
+  public static ArrayList<Byte> toArrayList(byte[] elems) {
+    ArrayList<Byte> list = new ArrayList<>();
+    for (byte e : elems)
+      list.add(e);
+    return list;
+  }
+
+  /**
+   * A convenience method for converting an array of primitive shorts to an ArrayList of Short
+   * objects.
+   *
+   * @param elems a primitive array of shorts
+   * @return an ArrayList of Short objects
+   */
+  public static ArrayList<Short> toArrayList(short[] elems) {
+    ArrayList<Short> list = new ArrayList<>();
+    for (short e : elems)
+      list.add(e);
+    return list;
+  }
+
+  /**
+   * A convenience method for converting an array of primitive integers to an ArrayList of Integer
+   * objects.
+   *
+   * @param elems a primitive array of integers
+   * @return an ArrayList of Integer objects
+   */
+  public static ArrayList<Integer> toArrayList(int[] elems) {
+    ArrayList<Integer> list = new ArrayList<>();
+    for (int e : elems)
+      list.add(e);
+    return list;
+  }
+
+  /**
+   * A convenience method for converting an array of primitive longs to an ArrayList of Long
+   * objects.
+   *
+   * @param elems a primitive array of longs
+   * @return an ArrayList of Long objects
+   */
+  public static ArrayList<Long> toArrayList(long[] elems) {
+    ArrayList<Long> list = new ArrayList<>();
+    for (long e : elems)
+      list.add(e);
+    return list;
+  }
+
+  /**
+   * A convenience method for converting an array of primitive floats to an ArrayList of Float
+   * objects.
+   *
+   * @param elems a primitive array of floats
+   * @return an ArrayList of Float objects
+   */
+  public static ArrayList<Float> toArrayList(float[] elems) {
+    ArrayList<Float> list = new ArrayList<>();
+    for (float e : elems)
+      list.add(e);
+    return list;
+  }
+
+  /**
+   * A convenience method for converting an array of primitive doubles to an ArrayList of Double
+   * objects.
+   *
+   * @param elems a primitive array of doubles
+   * @return an ArrayList of Double objects
+   */
+  public static ArrayList<Double> toArrayList(double[] elems) {
+    ArrayList<Double> list = new ArrayList<>();
+    for (double e : elems)
+      list.add(e);
+    return list;
+  }
+
+  /**
+   * A convenience method for converting an array of primitive booleans to an ArrayList of Boolean
+   * objects.
+   *
+   * @param elems a primitive array of booleans
+   * @return an ArrayList of Boolean objects
+   */
+  public static ArrayList<Boolean> toArrayList(boolean[] elems) {
+    ArrayList<Boolean> list = new ArrayList<>();
+    for (boolean e : elems)
+      list.add(e);
+    return list;
+  }
+
+  /**
+   * A convenience method for converting an array of primitive chars to an ArrayList of Character
+   * objects.
+   *
+   * @param elems a primitive array of chars
+   * @return an ArrayList of Character objects
+   */
+  public static ArrayList<Character> toArrayList(char[] elems) {
+    ArrayList<Character> list = new ArrayList<>();
+    for (char e : elems)
+      list.add(e);
+    return list;
   }
 
   public static void main(String[] args) {
